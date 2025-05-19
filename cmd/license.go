6b33604ec106329d02c04ef3eadcd57f14d6603d
Rafael Gomez <rgomez@redborder.com>
2025-05-19 11:48:19 +0100

    Update FormatLicense to reflect new limit bytes version 2.0.8

--- cmd/license.go
+++ cmd/license.go
@@ -249,7 +249,7 @@ func FormatLicense(license *License) string {
 		keyColor("\tCluster UUID:      ")+"%s\n"+
 		keyColor("\tOrganization UUID: ")+"%s\n"+
 		keyColor("\tExpires:           ")+"%s\n"+
-		keyColor("\tLimit bytes 2.0.7: ")+"%d",
+		keyColor("\tLimit bytes 2.0.8: ")+"%d",
 		license.UUID,
 		license.ClusterUUID,
 		license.Organization,
